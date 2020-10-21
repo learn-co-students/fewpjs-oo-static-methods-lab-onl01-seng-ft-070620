12bc2313c6ce87461e8c9114fbcdaff45a7170d3
JacksonReynolds <jwreynod@gmail.com>
2020-10-21 11:54:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    let newStr = str.match(/[a-zA-Z-' ]/g)
+    if (!!newStr) {return newStr.join('')} else {return ''}
+  }
+
+  static titleize(str) {
+    let leaves = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return str.split(' ').map((word, i) => {
+      if (!leaves.includes(word) || i === 0) {
+        return word[0].toUpperCase()+word.slice(1)
+      } else {
+        return word
+      }
+    }).join(' ')
+  }
+
 }
