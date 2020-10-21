493c93b2e5d5ad4ad9d051cc36c67cb0751a0693
wickbushong <wickbushong@gmail.com>
2020-10-21 16:10:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(str) {
+    return str.split(" ").map(function(w, i) {
+      if (!/^(the|a|an|but|of|and|for|at|by|from)$/.test(w) || i === 0) {
+        return Formatter.capitalize(w)
+      } else {
+        return w
+      }
+    }).join(" ")
+  }
+
 }
